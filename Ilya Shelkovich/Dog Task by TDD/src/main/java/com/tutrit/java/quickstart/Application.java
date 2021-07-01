9c3eebf816af82fb3d440ea282c4e887b392b57c
lamaconda <kennethoffi6.6@gmail.com>
2021-07-01 18:29:52 +0300

    did the dog task

--- Ilya Shelkovich/Dog Task by TDD/src/main/java/com/tutrit/java/quickstart/Application.java
+++ Ilya Shelkovich/Dog Task by TDD/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -1,9 +1,26 @@
 package com.tutrit.java.quickstart;
 
+import com.tutrit.java.quickstart.bean.Dog;
+import com.tutrit.java.quickstart.service.DogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+    static Logger log = LoggerFactory.getLogger("Application");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var bullDog = new Dog();
+        var dogService = new DogService();
+
+        bullDog.setAge(4);
+        bullDog.setColor("red");
+        bullDog.setName("Shrimp");
+
+        log.info("Dog created: {}", bullDog);
+
+        dogService.run();
+
     }
 }
 
