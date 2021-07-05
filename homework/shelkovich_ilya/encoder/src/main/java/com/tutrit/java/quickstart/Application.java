49a97dee636fa2b0fed16daee382898b5d0d84a6
lamaconda <kennethoffi6.6@gmail.com>
2021-07-05 16:07:06 +0300

    finished the encoder task

--- homework/shelkovich_ilya/encoder/src/main/java/com/tutrit/java/quickstart/Application.java
+++ homework/shelkovich_ilya/encoder/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -1,9 +1,14 @@
 package com.tutrit.java.quickstart;
 
+import com.tutrit.java.quickstart.service.Encoder;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var encoder = new Encoder();
+
+        encoder.encode((short) 65);
+        encoder.decode('z');
     }
 }
 
