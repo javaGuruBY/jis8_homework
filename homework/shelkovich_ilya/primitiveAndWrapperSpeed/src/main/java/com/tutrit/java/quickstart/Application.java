00def313859bb1692d1939829e06231eea795b55
lamaconda <kennethoffi6.6@gmail.com>
2021-07-12 00:22:52 +0300

    finished task, primitives are FASTER!

--- homework/shelkovich_ilya/primitiveAndWrapperSpeed/src/main/java/com/tutrit/java/quickstart/Application.java
+++ homework/shelkovich_ilya/primitiveAndWrapperSpeed/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -1,9 +1,18 @@
 package com.tutrit.java.quickstart;
 
+import com.tutrit.java.quickstart.service.Sum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+    static Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var sum = new Sum();
+
+        log.info("Time to count primitives {}", sum.timeForCountPrimitive());
+        log.info("Time to count wrapper {}", sum.timeForCountWrapper());
     }
 }
 
