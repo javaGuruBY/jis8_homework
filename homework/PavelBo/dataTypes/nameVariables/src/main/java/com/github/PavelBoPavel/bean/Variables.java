fd630ac5dcde736b0b7b536a6501d8d301f5309e
PS <pava.bore@gmail.com>
2021-07-14 23:20:34 +0300

    add name variables

---
+++ homework/PavelBo/dataTypes/nameVariables/src/main/java/com/github/PavelBoPavel/bean/Variables.java
@@ -0,0 +1,185 @@
+package com.github.PavelBoPavel.bean;
+
+public class Variables {
+
+    private int numberOfShipsInPort;
+    private int counterInteger;
+    private int firstNumber;
+    private String answerTheQuestion;
+    private double randomNumber;
+    private double randomNumberGenerator;
+    private String carColor;
+    private int speedCar;
+    private int numberEnteredUser;
+    private int  largestNumber;
+    private int smallestNumber;
+    private int leftBorderOfRangeOfNumbers;
+    private int rightBorderOfRangeOfNumbers;
+    private int numberUserGuessed;
+    private String versionAnswer;
+    private String bankAccount;
+    private String bankBalance;
+    private String creditCard;
+    private int numberOfWheelsOnBike;
+    private int ticketPrice;
+
+    public int getNumberOfShipsInPort() {
+        return numberOfShipsInPort;
+    }
+
+    public void setNumberOfShipsInPort(int numberOfShipsInPort) {
+        this.numberOfShipsInPort = numberOfShipsInPort;
+    }
+
+    public int getCounterInteger() {
+        return counterInteger;
+    }
+
+    public void setCounterInteger(int counterInteger) {
+        this.counterInteger = counterInteger;
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public String getAnswerTheQuestion() {
+        return answerTheQuestion;
+    }
+
+    public void setAnswerTheQuestion(String answerTheQuestion) {
+        this.answerTheQuestion = answerTheQuestion;
+    }
+
+    public double getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(double randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public double getRandomNumberGenerator() {
+        return randomNumberGenerator;
+    }
+
+    public void setRandomNumberGenerator(double randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public int getSpeedCar() {
+        return speedCar;
+    }
+
+    public void setSpeedCar(int speedCar) {
+        this.speedCar = speedCar;
+    }
+
+    public int getNumberEnteredUser() {
+        return numberEnteredUser;
+    }
+
+    public void setNumberEnteredUser(int numberEnteredUser) {
+        this.numberEnteredUser = numberEnteredUser;
+    }
+
+    public int getLargestNumber() {
+        return largestNumber;
+    }
+
+    public void setLargestNumber(int largestNumber) {
+        this.largestNumber = largestNumber;
+    }
+
+    public int getSmallestNumber() {
+        return smallestNumber;
+    }
+
+    public void setSmallestNumber(int smallestNumber) {
+        this.smallestNumber = smallestNumber;
+    }
+
+    public int getLeftBorderOfRangeOfNumbers() {
+        return leftBorderOfRangeOfNumbers;
+    }
+
+    public void setLeftBorderOfRangeOfNumbers(int leftBorderOfRangeOfNumbers) {
+        this.leftBorderOfRangeOfNumbers = leftBorderOfRangeOfNumbers;
+    }
+
+    public int getRightBorderOfRangeOfNumbers() {
+        return rightBorderOfRangeOfNumbers;
+    }
+
+    public void setRightBorderOfRangeOfNumbers(int rightBorderOfRangeOfNumbers) {
+        this.rightBorderOfRangeOfNumbers = rightBorderOfRangeOfNumbers;
+    }
+
+    public int getNumberUserGuessed() {
+        return numberUserGuessed;
+    }
+
+    public void setNumberUserGuessed(int numberUserGuessed) {
+        this.numberUserGuessed = numberUserGuessed;
+    }
+
+    public String getVersionAnswer() {
+        return versionAnswer;
+    }
+
+    public void setVersionAnswer(String versionAnswer) {
+        this.versionAnswer = versionAnswer;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getBankBalance() {
+        return bankBalance;
+    }
+
+    public void setBankBalance(String bankBalance) {
+        this.bankBalance = bankBalance;
+    }
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public int getNumberOfWheelsOnBike() {
+        return numberOfWheelsOnBike;
+    }
+
+    public void setNumberOfWheelsOnBike(int numberOfWheelsOnBike) {
+        this.numberOfWheelsOnBike = numberOfWheelsOnBike;
+    }
+
+    public int getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(int ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+}
