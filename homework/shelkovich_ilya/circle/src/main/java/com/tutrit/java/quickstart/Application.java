37bc64bd35856458842a39428f9da685a25b645a
lamaconda <kennethoffi6.6@gmail.com>
2021-07-15 20:40:13 +0300

    Improved method in CircleService, refactored test, added constructor in Circle class and deleted redundant code.

--- homework/shelkovich_ilya/circle/src/main/java/com/tutrit/java/quickstart/Application.java
+++ homework/shelkovich_ilya/circle/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -3,7 +3,7 @@ package com.tutrit.java.quickstart;
 public class Application {
 
     public static void main(String[] args) {
-        // code is redundant
+        // code is redundant!
     }
 }
 
