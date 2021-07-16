f1a862d4c445a93113f36d972607a4e616d9b9b6
lamaconda <kennethoffi6.6@gmail.com>
2021-07-16 16:10:25 +0300

    replaced code from main to test

--- homework/shelkovich_ilya/wrapperClasses/src/test/java/com/tutrit/java/quickstart/service/IntegerServiceTest.java
+++ homework/shelkovich_ilya/wrapperClasses/src/test/java/com/tutrit/java/quickstart/service/IntegerServiceTest.java
@@ -1,15 +1,59 @@
 package com.tutrit.java.quickstart.service;
 
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class IntegerServiceTest {
 
-    Integer a;
-    Integer b;
+    @Test
+    public void equalsWithCache(){
+        Integer a = 20;
+        Integer b = 20;
 
-    @Before
-    public void setUp(){
-        this.a = 20;
-        this.b = 20;
+        boolean result = a == b;
+        Assert.assertTrue(result);
+        boolean resultByEquals = a.equals(b);
+        Assert.assertTrue(resultByEquals);
+    }
+
+    @Test
+    public void equalsWithNonCacheRightBorder(){
+        Integer a = 200;
+        Integer b = 200;
+
+        boolean result = a == b;
+        Assert.assertFalse(result);
+        boolean resultByEquals = a.equals(b);
+        Assert.assertTrue(resultByEquals);
+    }
+
+    @Test
+    public void equalsWithNonCacheLeftBorder(){
+        Integer a = -200;
+        Integer b = -200;
+
+        boolean result = a == b;
+        Assert.assertFalse(result);
+        boolean resultByEquals = a.equals(b);
+        Assert.assertTrue(resultByEquals);
+    }
+
+    @Test
+    public void equalsWithCacheInBorder(){
+        Integer a = 127;
+        Integer b = 127;
+
+        boolean result = a == b;
+        Assert.assertTrue(result);
+        boolean resultByEquals = a.equals(b);
+        Assert.assertTrue(resultByEquals);
+
+        Integer c = -128;
+        Integer d = -128;
+
+        boolean result2 = c == d;
+        Assert.assertTrue(result2);
+        boolean resultByEquals2 = c.equals(d);
+        Assert.assertTrue(resultByEquals2);
     }
 }
