a25b83a3e46ad49e426575774dd60177122c0cd4
lamaconda <kennethoffi6.6@gmail.com>
2021-07-17 12:12:56 +0300

    Modified array assign

--- homework/shelkovich_ilya/lookUpArrayService/src/test/java/com/tutrit/java/quickstart/service/LookUpArrayServiceTest.java
+++ homework/shelkovich_ilya/lookUpArrayService/src/test/java/com/tutrit/java/quickstart/service/LookUpArrayServiceTest.java
@@ -16,21 +16,21 @@ public class LookUpArrayServiceTest {
 
     @Test
     public void findMax() {
-        int[] array = new int[]{1, 3, 12, 4};
+        int[] array = {1, 3, 12, 4};
 
         Assert.assertEquals(12, lookUpArrayService.findMax(array));
     }
 
     @Test
     public void findMin() {
-        int[] array = new int[]{1, 3, 12, 4};
+        int[] array = {1, 3, 12, 4};
 
         Assert.assertEquals(1, lookUpArrayService.findMin(array));
     }
 
     @Test
     public void shouldReturn0IfArrayIsEmpty() {
-        int[] emptyArray = new int[]{};
+        int[] emptyArray = {};
 
         Assert.assertEquals(0, lookUpArrayService.findMax(emptyArray));
         Assert.assertEquals(0, lookUpArrayService.findMin(emptyArray));
@@ -38,28 +38,28 @@ public class LookUpArrayServiceTest {
 
     @Test
     public void indexOfMax() {
-        int[] array = new int[]{1, 3, 12, 4};
+        int[] array = {1, 3, 12, 4};
 
         Assert.assertEquals(2, lookUpArrayService.indexOfMax(array));
     }
 
     @Test
     public void indexOfMin() {
-        int[] array = new int[]{1, 3, 12, 4};
+        int[] array = {1, 3, 12, 4};
 
         Assert.assertEquals(0, lookUpArrayService.indexOfMin(array));
     }
 
     @Test
     public void indexOf() {
-        int[] array = new int[]{1, 3, 12, 4};
+        int[] array = {1, 3, 12, 4};
 
         Assert.assertEquals(2, lookUpArrayService.indexOf(array, 12));
     }
 
     @Test
     public void shouldReturnMinusOneIfArrayIsEmpty() {
-        int[] emptyArray = new int[]{};
+        int[] emptyArray = {};
 
         Assert.assertEquals(-1, lookUpArrayService.indexOfMax(emptyArray));
         Assert.assertEquals(-1, lookUpArrayService.indexOfMin(emptyArray));
