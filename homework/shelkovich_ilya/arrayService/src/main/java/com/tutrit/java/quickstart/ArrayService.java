9785c53a2ec6ee8fb3cbe564d86c076586d48da9
lamaconda <kennethoffi6.6@gmail.com>
2021-07-18 14:26:39 +0300

    added 2 methods, first is creating empty array, second is fills provided array with random numbers from 0 to 100

--- homework/shelkovich_ilya/arrayService/src/main/java/com/tutrit/java/quickstart/ArrayService.java
+++ homework/shelkovich_ilya/arrayService/src/main/java/com/tutrit/java/quickstart/ArrayService.java
@@ -1,6 +1,16 @@
 package com.tutrit.java.quickstart;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ArrayService {
 
+    public int[] createArray(int size){
+        return new int[size];
+    }
 
+    public int[] fillRandomly(int[] array){
+        for (int i = 0; i < array.length; i++){
+            array[i] = ThreadLocalRandom.current().nextInt(-1, 101);
+        } return array;
+    }
 }
---
+++ homework/shelkovich_ilya/arrayService/src/test/java/com/tutrit/java/quickstart/ArrayServiceTest.java
@@ -0,0 +1,29 @@
+package com.tutrit.java.quickstart;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayServiceTest {
+
+    ArrayService arrayService;
+
+    @Before
+    public void setUp(){
+        this.arrayService = new ArrayService();
+    }
+
+    @Test
+    public void createArray() {
+        Assert.assertArrayEquals(new int[]{}, arrayService.createArray(0));
+    }
+
+    // не уверен что этот тест проверяет то, что я хотел. Я хотел проверить заполняется ли массив случайными числами
+    @Test
+    public void fillRandomly(){
+        int[] array = new int[10];
+        Assert.assertArrayEquals(array, arrayService.fillRandomly(array));
+    }
+
+
+}
