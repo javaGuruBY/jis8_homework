4dc2676489e8dbaa6715b705ab3e1bf659a40722
lamaconda <kennethoffi6.6@gmail.com>
2021-07-18 21:06:43 +0300

    finished palindrome task, unfortunately not like I wanted to solve this task

--- homework/shelkovich_ilya/palindrome/src/main/java/com/tutrit/java/quickstart/service/Palindrome.java
+++ homework/shelkovich_ilya/palindrome/src/main/java/com/tutrit/java/quickstart/service/Palindrome.java
@@ -4,11 +4,9 @@ public class Palindrome {
 
     public boolean isPalindrome(String text) {
         var stringBuilder = new StringBuilder(text);
-
         stringBuilder.reverse();
 
         var reverseText = stringBuilder.toString();
-
         return text.equals(reverseText);
     }
 }
