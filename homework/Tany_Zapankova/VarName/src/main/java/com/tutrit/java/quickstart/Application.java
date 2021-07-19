d7ac5eff26a8e4f931dd19c794aac4d4a751cbfb
Puzzletania <puzzletania@yandex.ru>
2021-07-19 21:33:33 +0300

    Требования: Придумать названия для переменных

---
+++ homework/Tany_Zapankova/VarName/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -0,0 +1,28 @@
+package com.tutrit.java.quickstart;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        int shipsAmount;
+        int intCounter;
+        int firstNumber;
+        int randomNumber;
+        int autoSpeed;
+        int userEnteredNumber;
+        int largestNumber;
+        double smallerNumber;
+        int leftBoardInt;
+        int rightBoardInt;
+        int userGuessedNumber;
+        float cashBalance;
+        byte bikeWheels;
+        boolean isHungry;
+        char latinSymbol;
+        boolean evenNumber;
+        int year;
+        char space;
+        float ticketPrice;
+    }
+}
+
