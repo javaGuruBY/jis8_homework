d4e5b11ded535dcea38f95c8ee25fac0191b1688
lamaconda <kennethoffi6.6@gmail.com>
2021-07-19 18:42:38 +0300

    finished butterfly task

--- homework/shelkovich_ilya/butterfly/src/main/java/com/tutrit/java/quickstart/Application.java
+++ homework/shelkovich_ilya/butterfly/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -1,9 +1,30 @@
 package com.tutrit.java.quickstart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger log = LoggerFactory.getLogger("");
+
+        int[][] butterfly = {
+                {1, 1, 1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, 1, 1}
+        };
+
+
+        for (int[] ints : butterfly) {
+            log.info("butterfly: {} {} {} {} {} {} {}", ints[0], ints[1], ints[2], ints[3],
+                    ints[4], ints[5], ints[6]);
+        }
     }
 }
+
+
 
