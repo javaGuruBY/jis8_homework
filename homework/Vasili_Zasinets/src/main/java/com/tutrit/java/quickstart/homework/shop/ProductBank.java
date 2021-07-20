bb78d2bf247154bbdc81b989fb256b0dff970a7e
VasiliZas <vasili.zasinets@gmail.com>
2021-07-20 22:29:29 +0300

    Shop Task. Change all my code

---
+++ homework/Vasili_Zasinets/src/main/java/com/tutrit/java/quickstart/homework/shop/ProductBank.java
@@ -0,0 +1,13 @@
+package com.tutrit.java.quickstart.homework.shop;
+
+import java.math.BigDecimal;
+
+public class ProductBank {
+    Product milk = new Product("Milk", BigDecimal.valueOf(15.89));
+    Product water = new Product("water", BigDecimal.valueOf(1.01));
+    Product bread = new Product("bread", BigDecimal.valueOf(0.19));
+    Product meal = new Product("meal", BigDecimal.valueOf(2.19));
+    Product cola = new Product("cola", BigDecimal.valueOf(4.59));
+    Product pepsi = new Product("pepsi", BigDecimal.valueOf(1.99));
+
+}
---
+++ homework/Vasili_Zasinets/src/main/java/com/tutrit/java/quickstart/homework/shop/Shop.java
@@ -0,0 +1,49 @@
+package com.tutrit.java.quickstart.homework.shop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Shop {
+    Logger log = LoggerFactory.getLogger("Shop ");
+    Map<String, BigDecimal> myShop = new LinkedHashMap<>();
+    Product myProduct;
+    ProductBank bank;
+
+    @Override
+    public String toString() {
+        return " " +
+                "shop =" + myShop +
+                '}';
+    }
+
+    void addProduct(Product product) {
+        myShop.put(product.getName(), product.getPrice());
+
+    }
+
+    void deleteProduct(Product product) {
+        myShop.remove(product.getName());
+    }
+
+    void searchProductOfName(String name) {
+        log.info("Product: {}", name + ", price: " + myShop.get(name));
+    }
+
+
+    void findProduct(BigDecimal minPriceRange, BigDecimal maxPriceRange) {
+        for (Map.Entry<String, BigDecimal> entry : myShop.entrySet()) {
+            String k = entry.getKey();
+            BigDecimal v = entry.getValue();
+            if (v.compareTo(maxPriceRange) <= 0 && v.compareTo(minPriceRange) >= 0) {
+                log.info("Product : {}", k + " price: " + myShop.get(k));
+
+            }
+
+        }
+    }
+}
+
