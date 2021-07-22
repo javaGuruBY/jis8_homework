900e0a79ff493393d281c247d87ca8e447c2d9ba
PS <pava.bore@gmail.com>
2021-07-22 17:31:09 +0300

    add 3 loops

---
+++ homework/PavelBo/loops/forWhileDoWhile/src/test/java/com/tutrit/java/quickstart/DoWhileOddCheckerTest.java
@@ -0,0 +1,13 @@
+package com.tutrit.java.quickstart;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DoWhileOddCheckerTest {
+
+    @Test
+    public void oddChecker() {
+        DoWhileOddChecker.oddChecker();
+    }
+}
---
+++ homework/PavelBo/loops/forWhileDoWhile/src/test/java/com/tutrit/java/quickstart/WhileOddCheckerTest.java
@@ -0,0 +1,13 @@
+package com.tutrit.java.quickstart;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WhileOddCheckerTest {
+
+    @Test
+    public void oddChecker() {
+        WhileOddChecker.oddChecker();
+    }
+}
