d57c3ca1a1e8bf3adec3415a284d2aa7d4fefd61
lamaconda <kennethoffi6.6@gmail.com>
2021-07-28 18:33:03 +0300

    Improved CircleServiceTest.

--- homework/shelkovich_ilya/circle/src/test/java/com/tutrit/java/quickstart/service/CircleServiceTest.java
+++ homework/shelkovich_ilya/circle/src/test/java/com/tutrit/java/quickstart/service/CircleServiceTest.java
@@ -7,20 +7,19 @@ import org.junit.Test;
 
 public class CircleServiceTest {
 
-    Circle circle;
     CircleService circleService;
 
     @Before
     public void setUp(){
-        this.circle = new Circle(4);
         this.circleService = new CircleService();
     }
 
     @Test
     public void calculateArea() {
-        double expected = Math.PI * circle.getRadius() * circle.getRadius();
+        Circle circle = new Circle(4);
+        double expected = 50.26;
         double actual = circleService.calculateArea(circle);
-        double delta = 0;
+        double delta = 2;
         Assert.assertEquals(expected, actual, delta);
     }
 }
