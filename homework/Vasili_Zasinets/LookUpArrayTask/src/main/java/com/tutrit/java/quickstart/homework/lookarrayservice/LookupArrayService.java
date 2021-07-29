a40776ab550c55eb127b331c0a51e01067622148
VasiliZas <vasili.zasinets@gmail.com>
2021-07-29 22:15:06 +0300

    fixed. use sonar and quickstart

--- homework/Vasili_Zasinets/LookUpArrayTask/src/main/java/com/tutrit/java/quickstart/homework/lookarrayservice/LookupArrayService.java
+++ homework/Vasili_Zasinets/LookUpArrayTask/src/main/java/com/tutrit/java/quickstart/homework/lookarrayservice/LookupArrayService.java
@@ -7,8 +7,12 @@ public class LookupArrayService {
     static Logger log = LoggerFactory.getLogger("Array");
 
     public int findMax(int[] array) {
-        if (array == null) return 0;
-       int arrayMax = array[0];
+        try {
+            checkIfArrayEmpty(array);
+        } catch (ArrayEmptyException exception) {
+            return 0;
+        }
+        int arrayMax = array[0];
         for (int j : array) {
             if (arrayMax < j) {
                 arrayMax = j;
@@ -19,8 +23,12 @@ public class LookupArrayService {
     }
 
     public int findMin(int[] array) {
-        if (array == null) return 0;
-       int arrayMin = array[0];
+        try {
+            checkIfArrayEmpty(array);
+        } catch (ArrayEmptyException exception) {
+            return 0;
+        }
+        int arrayMin = array[0];
         for (int j : array) {
             if (arrayMin > j) {
                 arrayMin = j;
@@ -28,13 +36,16 @@ public class LookupArrayService {
         }
         log.info("arrayMin = {}", arrayMin);
         return arrayMin;
-
     }
 
     public int indexOfMax(int[] array) {
         int indexMax = -1;
-        if (array == null) return -1;
-       int arrayMax = array[0];
+        try {
+            checkIfArrayEmpty(array);
+        } catch (ArrayEmptyException exception) {
+            return -1;
+        }
+        int arrayMax = array[0];
         for (int i = 0; i < array.length; i++) {
             if (arrayMax < array[i]) {
                 arrayMax = array[i];
@@ -48,12 +59,16 @@ public class LookupArrayService {
 
     public int indexOfMin(int[] array) {
         int indexMin = -1;
-        if (array == null) return -1;
-       int arrayMin = array[0];
+        try {
+            checkIfArrayEmpty(array);
+        } catch (ArrayEmptyException exception) {
+            return -1;
+        }
+        int arrayMin = array[0];
         for (int i = 0; i < array.length; i++) {
             if (arrayMin > array[i]) {
                 arrayMin = array[i];
-               indexMin = i;
+                indexMin = i;
             }
         }
         log.info("indexMin = {}", indexMin);
@@ -62,11 +77,22 @@ public class LookupArrayService {
     }
 
     public int indexOf(int[] array, int value) {
+        try {
+            checkIfArrayEmpty(array);
+        } catch (ArrayEmptyException exception) {
+            return -1;
+        }
         int i = 0;
         while (value != array[i]) {
             i++;
         }
         log.info("indexOf = {}", i);
         return i;
+    }
+
+    public void checkIfArrayEmpty(int[] array) throws ArrayEmptyException {
+        if (array == null || array.length == 0) {
+            throw new ArrayEmptyException("Array could not be empty or zero");
+        }
     }
 }
