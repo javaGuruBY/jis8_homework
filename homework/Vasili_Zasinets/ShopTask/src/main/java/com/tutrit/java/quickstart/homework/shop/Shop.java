2bc6e5a7f51dc62bec9a417a02306a96070d622d
VasiliZas <vasili.zasinets@gmail.com>
2021-07-29 22:36:51 +0300

    use quickstart and sonar

---
+++ homework/Vasili_Zasinets/ShopTask/src/main/java/com/tutrit/java/quickstart/homework/shop/Shop.java
@@ -0,0 +1,44 @@
+package com.tutrit.java.quickstart.homework.shop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Shop {
+    Logger log = LoggerFactory.getLogger("Shop");
+    Map<String, BigDecimal> myShop = new LinkedHashMap<>();
+
+
+    public void addProduct(Product product) {
+        myShop.put(product.getName(), product.getPrice());
+    }
+
+   public void deleteProduct(Product product) {
+        myShop.remove(product.getName());
+    }
+
+    public void searchProductOfName(String name) {
+        log.info("Product: {}, price{}", name, myShop.get(name));
+    }
+
+    public void findProduct(BigDecimal minPriceRange, BigDecimal maxPriceRange) {
+        for (Map.Entry<String, BigDecimal> entry : myShop.entrySet()) {
+            String k = entry.getKey();
+            BigDecimal v = entry.getValue();
+            if (v.compareTo(maxPriceRange) <= 0 && v.compareTo(minPriceRange) >= 0) {
+                log.info("Product : {}, price {}", k ,myShop.get(k));
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return " " +
+                "shop =" + myShop +
+                '}';
+    }
+}
+
---
+++ homework/Vasili_Zasinets/ShopTask/src/test/java/com/tutrit/java/quickstart/homework/shop/ShopTest.java
@@ -0,0 +1,47 @@
+package com.tutrit.java.quickstart.homework.shop;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+
+public class ShopTest {
+    ProductRepository bank;
+
+    Shop myTestShop = new Shop();
+    Logger log = LoggerFactory.getLogger("Test ");
+
+    @Before
+    public void setUp() {
+        this.bank = new ProductRepository();
+        myTestShop.addProduct(bank.milk);
+        myTestShop.addProduct(bank.water);
+        myTestShop.addProduct(bank.bread);
+        myTestShop.addProduct(bank.cola);
+        myTestShop.addProduct(bank.meal);
+    }
+
+    @Test
+    public void addProduct() {
+        myTestShop.addProduct(bank.pepsi);
+        log.info("Product in shop : {}", myTestShop);
+    }
+
+    @Test
+    public void deleteProduct() {
+        myTestShop.deleteProduct(bank.water);
+        log.info("Product  : {}", myTestShop);
+    }
+
+    @Test
+    public void searchProductOfName() {
+        myTestShop.searchProductOfName("bread");
+    }
+
+    @Test
+    public void findProduct() {
+        myTestShop.findProduct(BigDecimal.valueOf(2), BigDecimal.valueOf(16));
+    }
+}
