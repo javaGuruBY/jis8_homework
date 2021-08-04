f9040ea9a98a14757bb8491e85f65bd0db31e1e5
lamaconda <kennethoffi6.6@gmail.com>
2021-08-04 17:41:38 +0300

    created user

---
+++ homework/shelkovich_ilya/userValidationService/src/main/java/com/tutrit/java/quickstart/bean/User.java
@@ -0,0 +1,58 @@
+package com.tutrit.java.quickstart.bean;
+
+import java.util.Objects;
+
+public class User {
+    String firstName;
+    String lastName;
+    int age;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
