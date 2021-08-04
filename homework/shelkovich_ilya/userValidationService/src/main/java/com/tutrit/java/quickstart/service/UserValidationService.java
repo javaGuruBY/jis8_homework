e47d7b66cd9316b7cbbd94eeb17b3a42660f82dc
lamaconda <kennethoffi6.6@gmail.com>
2021-08-04 18:08:46 +0300

    added userValidationService functionality

--- homework/shelkovich_ilya/userValidationService/src/main/java/com/tutrit/java/quickstart/service/UserValidationService.java
+++ homework/shelkovich_ilya/userValidationService/src/main/java/com/tutrit/java/quickstart/service/UserValidationService.java
@@ -1,6 +1,42 @@
 package com.tutrit.java.quickstart.service;
 
+import com.tutrit.java.quickstart.bean.User;
+import com.tutrit.java.quickstart.exceptions.UserValidationException;
+
 public class UserValidationService {
+
+    public boolean validate(User user) throws UserValidationException {
+        try {
+            checkUserNameLength(user);
+        } catch (UserValidationException nameException) {
+            return false;
+        }
+        try {
+            checkUserAge(user);
+        } catch (UserValidationException ageException) {
+            return false;
+        }
+        return true;
+    }
+
+    public void checkUserNameLength(User user) throws UserValidationException {
+        if (user.getFirstName().length() < 3 || user.getLastName().length() < 3) {
+            throw new UserValidationException("First or Last name is too short");
+        } else if (user.getFirstName().length() > 15 || user.getLastName().length() > 15) {
+            throw new UserValidationException("First or Last name is too long");
+        }
+    }
+
+    public void checkUserAge(User user) throws UserValidationException {
+        if (user.getAge() < 0 || user.getAge() > 120) {
+            throw new UserValidationException("Age is incorrect");
+        }
+    }
+
+
+
+//    Допустимый возраст пользователя: от 0 до 120 лет включительно;
+//    В случае, если данные не проходят одну из проверок, то необходимо выбросить UserValidationException с причиной ошибки.
 
 
 }
---
+++ homework/shelkovich_ilya/userValidationService/src/test/java/com/tutrit/java/quickstart/service/UserValidationServiceTest.java
@@ -0,0 +1,12 @@
+package com.tutrit.java.quickstart.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserValidationServiceTest {
+
+    @Test
+    public void validate() {
+    }
+}
