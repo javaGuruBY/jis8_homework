f5df325b00cdeffaf69785d9334bfbea256174e8
KaydunovDenis <deniskaydunov@gmail.com>
2021-08-08 14:39:17 +0300

    has been change pom.xml and tests delete dependency -  jupiter

--- homework/kaydunov_denis/task1/src/test/java/github/kaydunovdenis/service/ProductValidateServiceTest.java
+++ homework/kaydunov_denis/task1/src/test/java/github/kaydunovdenis/service/ProductValidateServiceTest.java
@@ -1,66 +1,65 @@
 package github.kaydunovdenis.service;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-@SuppressWarnings("JUnit5AssertionsConverter")
-class ProductValidateServiceTest {
+public class ProductValidateServiceTest {
     private ProductValidateService productValidateService;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         productValidateService = new ProductValidateService();
     }
 
     @Test
-    void validatePrice() {
+    public void validatePrice() {
         BigDecimal price = new BigDecimal("-12.3");
-        Assertions.assertFalse(productValidateService.validatePrice(price));
+        Assert.assertFalse(productValidateService.validatePrice(price));
 
         price = new BigDecimal("0");
-        Assertions.assertFalse(productValidateService.validatePrice(price));
+        Assert.assertFalse(productValidateService.validatePrice(price));
 
         price = new BigDecimal("10.032");
-        Assertions.assertTrue(productValidateService.validatePrice(price));
+        assertTrue(productValidateService.validatePrice(price));
     }
 
     @Test
-    void validateDiscount() {
+    public void validateDiscount() {
         BigDecimal discount = new BigDecimal("-12.3");
-        Assertions.assertFalse(productValidateService.validateDiscount(discount));
+        Assert.assertFalse(productValidateService.validateDiscount(discount));
 
         discount = new BigDecimal("0.0");
-        Assert.assertTrue(productValidateService.validateDiscount(discount));
+        assertTrue(productValidateService.validateDiscount(discount));
 
         discount = new BigDecimal("11.32");
-        Assert.assertTrue(productValidateService.validateDiscount(discount));
+        assertTrue(productValidateService.validateDiscount(discount));
 
         discount = new BigDecimal("100.0");
-        Assert.assertTrue(productValidateService.validateDiscount(discount));
+        assertTrue(productValidateService.validateDiscount(discount));
 
         discount = new BigDecimal("111.32");
         assertFalse(productValidateService.validateDiscount(discount));
     }
 
     @Test
-    void validateProductName() {
+    public void validateProductName() {
         String name = "Pr";
         assertFalse(productValidateService.validateProductName(name));
 
         name = "Pro";
-        Assert.assertTrue(productValidateService.validateProductName(name));
+        assertTrue(productValidateService.validateProductName(name));
 
         name = "ProductName";
-        Assert.assertTrue(productValidateService.validateProductName(name));
+        assertTrue(productValidateService.validateProductName(name));
 
         name = "SomeProductNameIs_Equal_32_Chars";
-        Assert.assertTrue(productValidateService.validateProductName(name));
+        assertTrue(productValidateService.validateProductName(name));
 
         name = "SomeProductNameIsMoreThan32_Chars";
         assertFalse(productValidateService.validateProductName(name));
