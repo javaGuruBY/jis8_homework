b1a6bb63f65109603d09e194f946addf37d7d776
KaydunovDenis <deniskaydunov@gmail.com>
2021-08-08 11:44:11 +0300

    create package with my homework add task1 ShoppingListApplication

---
+++ homework/kaydunov_denis/task1/src/main/java/github/kaydunovdenis/ShoppingListApplication.java
@@ -0,0 +1,13 @@
+package github.kaydunovdenis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShoppingListApplication {
+    private static Logger log = LoggerFactory.getLogger("main");
+
+    public static void main(String[] args) {
+        log.info("Hello world!!");
+    }
+}
+
