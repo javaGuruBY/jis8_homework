384232c80a97bf20f6882107acf4596ce75e46b5
VasiliZas <vasili.zasinets@gmail.com>
2021-08-14 15:26:48 +0300

    fixed- rename metod

--- homework/Vasili_Zasinets/Shape/src/test/java/com/tutrit/java/quickstart/shape/CircleTest.java
+++ homework/Vasili_Zasinets/Shape/src/test/java/com/tutrit/java/quickstart/shape/CircleTest.java
@@ -2,34 +2,34 @@ package com.tutrit.java.quickstart.shape;
 
 import org.junit.Test;
 
-import static com.tutrit.java.quickstart.shape.facade.Facade.facadeForShape;
+import static com.tutrit.java.quickstart.shape.facade.Facade.getNewShape;
 import static org.junit.Assert.assertEquals;
 
 public class CircleTest {
 
-    //facadeForShape(String typeShape, String nameShape, double value) {
+    //getNewShape(String typeShape, String nameShape, double value) {
     @Test
     public void lengthOfFigureBorders() throws ShapeException {
-        assertEquals(37.7, facadeForShape("Circle", "CircleTest", 6).lengthOfFigureBorders(), 0.1);
+        assertEquals(37.7, getNewShape("Circle", "CircleTest", 6).lengthOfFigureBorders(), 0.1);
     }
 
     @Test
     public void getName() {
-        assertEquals("CircleTest", facadeForShape("Circle", "CircleTest", 6).getName());
+        assertEquals("CircleTest", getNewShape("Circle", "CircleTest", 6).getName());
     }
 
     @Test
     public void getArea() throws ShapeException {
-        assertEquals(78.54, facadeForShape("Circle", "CircleTest", 5).getArea(), 0.01);
+        assertEquals(78.54, getNewShape("Circle", "CircleTest", 5).getArea(), 0.01);
     }
 
     @Test(expected = ShapeException.class)
     public void lengthOfFigureBordersException() throws ShapeException {
-        facadeForShape("Circle", "CircleTest", -6).lengthOfFigureBorders();
+        getNewShape("Circle", "CircleTest", -6).lengthOfFigureBorders();
     }
 
     @Test(expected = ShapeException.class)
     public void getAreaException() throws ShapeException {
-        facadeForShape("Circle", "CircleTest", -7).getArea();
+        getNewShape("Circle", "CircleTest", -7).getArea();
     }
 }
--- homework/Vasili_Zasinets/Shape/src/test/java/com/tutrit/java/quickstart/shape/SquareTest.java
+++ homework/Vasili_Zasinets/Shape/src/test/java/com/tutrit/java/quickstart/shape/SquareTest.java
@@ -2,34 +2,34 @@ package com.tutrit.java.quickstart.shape;
 
 import org.junit.Test;
 
-import static com.tutrit.java.quickstart.shape.facade.Facade.facadeForShape;
+import static com.tutrit.java.quickstart.shape.facade.Facade.getNewShape;
 import static org.junit.Assert.assertEquals;
 
 public class SquareTest {
 
-    //facadeForShape(String typeShape, String nameShape, double value) {
+    //getNewShape(String typeShape, String nameShape, double value) {
     @Test
     public void getArea() throws ShapeException {
-        assertEquals(25, facadeForShape("Square", "SquareTest", 5).getArea(), 0.000);
+        assertEquals(25, getNewShape("Square", "SquareTest", 5).getArea(), 0.000);
     }
 
     @Test
     public void lengthOfFigureBorders() throws ShapeException {
-        assertEquals(24, facadeForShape("Square", "SquareTest", 6).lengthOfFigureBorders(),0.000);
+        assertEquals(24, getNewShape("Square", "SquareTest", 6).lengthOfFigureBorders(),0.000);
     }
 
     @Test
     public void getName() {
-        assertEquals("SquareTest", facadeForShape("Square", "SquareTest", 5).getName());
+        assertEquals("SquareTest", getNewShape("Square", "SquareTest", 5).getName());
     }
 
     @Test(expected = ShapeException.class)
     public void lengthOfFigureBordersException() throws ShapeException {
-        facadeForShape("Square", "SquareTest", -5).lengthOfFigureBorders();
+        getNewShape("Square", "SquareTest", -5).lengthOfFigureBorders();
     }
 
     @Test(expected = ShapeException.class)
     public void getAreaException() throws ShapeException {
-        facadeForShape("Square", "SquareTest", -5).getArea();
+        getNewShape("Square", "SquareTest", -5).getArea();
     }
 }
