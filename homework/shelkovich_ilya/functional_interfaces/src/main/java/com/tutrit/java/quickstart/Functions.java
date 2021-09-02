12424c084cf59601543703742d7c1d3099ff5b73
lamaconda <kennethoffi6.6@gmail.com>
2021-09-02 03:09:11 +0300

    functional interfaces repeated

---
+++ homework/shelkovich_ilya/functional_interfaces/src/main/java/com/tutrit/java/quickstart/Functions.java
@@ -0,0 +1,29 @@
+package com.tutrit.java.quickstart;
+
+import com.tutrit.java.quickstart.bean.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.function.*;
+
+public class Functions {
+
+    private Functions() {
+
+    }
+
+    static Logger log = LoggerFactory.getLogger(Functions.class);
+
+    public static final Runnable printCurrentTime = () -> log.info("{}", LocalDateTime.now());
+
+    public static final Supplier<Person> defaultPerson = () -> new Person("default", 18);
+    public static final Consumer<Person> printPerson = person -> log.info("{}", person);
+    public static final Function<String, Person> createPersonWithName = name -> new Person(name, 18);
+    public static final IntFunction<Person> createPersonWithAge = age -> new Person("default", age);
+    public static final UnaryOperator<Person> makePersonBrutallyOlder = person -> person.setAge(99);
+    public static final Predicate<Person> isPersonOld = person -> person.getAge() > 19;
+    public static final BiFunction<Person, Integer, Person> addAge = (p, i) -> p.setAge(p.getAge() + i);
+    public static final BiFunction<Person, Integer, Person> setAge = Person::setAge;
+    public static final BiFunction<Person, String, Person> setName = Person::setName;
+}
---
+++ homework/shelkovich_ilya/functional_interfaces/src/test/java/com/tutrit/java/quickstart/FunctionsTest.java
@@ -0,0 +1,69 @@
+package com.tutrit.java.quickstart;
+
+import com.tutrit.java.quickstart.bean.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.tutrit.java.quickstart.Functions.*;
+
+public class FunctionsTest {
+
+    @Test
+    public void printTime() {
+        printCurrentTime.run();
+    }
+
+    @Test
+    public void createDefaultPerson() {
+        Assert.assertEquals(new Person("default", 18), defaultPerson.get());
+    }
+
+    @Test
+    public void printPerson() {
+        printPerson.accept(defaultPerson.get());
+    }
+
+    @Test
+    public void createPersonWith() {
+        Person person1 = createPersonWithName.apply("Mark");
+        Assert.assertEquals("Mark", person1.getName());
+
+        Person person2 = createPersonWithAge.apply(49);
+        Assert.assertEquals(49, person2.getAge());
+    }
+
+    @Test
+    public void makePersonOlder() {
+        Person person = defaultPerson.get();
+        Assert.assertEquals(person.setAge(99), makePersonBrutallyOlder.apply(person));
+    }
+
+    @Test
+    public void isPersonOld() {
+        Person person = createPersonWithAge.apply(15);
+        Assert.assertFalse(isPersonOld.test(person));
+        person = makePersonBrutallyOlder.apply(person);
+        Assert.assertTrue(isPersonOld.test(person));
+    }
+
+    @Test
+    public void addAge() {
+        Person person = defaultPerson.get();
+        addAge.apply(person, 30);
+        Assert.assertEquals(48, person.getAge());
+    }
+
+    @Test
+    public void setAge() {
+        Person person = defaultPerson.get();
+        setAge.apply(person, 45);
+        Assert.assertEquals(45, person.getAge());
+    }
+
+    @Test
+    public void setName() {
+        Person person = defaultPerson.get();
+        setName.apply(person, "Mark");
+        Assert.assertEquals("Mark", person.getName());
+    }
+}
