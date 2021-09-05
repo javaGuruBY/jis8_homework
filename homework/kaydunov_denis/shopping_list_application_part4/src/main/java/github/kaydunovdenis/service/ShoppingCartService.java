be93839680e06e2bb9286dfc62d0930f404f7a21
KaydunovDenis <deniskaydunov@gmail.com>
2021-09-05 11:07:08 +0300

    Fixed some part of mistakes.

--- homework/kaydunov_denis/shopping_list_application_part4/src/main/java/github/kaydunovdenis/service/ShoppingCartService.java
+++ homework/kaydunov_denis/shopping_list_application_part4/src/main/java/github/kaydunovdenis/service/ShoppingCartService.java
@@ -6,16 +6,20 @@ import github.kaydunovdenis.bean.ShoppingCart;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
+    private ProductService productService;
+
+    public ShoppingCartService() {
+        this.productService = new ProductService();
+    }
 
     public void addProduct(final ShoppingCart shoppingCart, final Product product) {
         shoppingCart.getProductList().add(product);
     }
 
     public BigDecimal calculateTotalPrice(final ShoppingCart shoppingCart) {
-        ProductService productService = new ProductService();
-        BigDecimal totalPrice = new BigDecimal("0.00");
+        var totalPrice = new BigDecimal("0.00");
         for (Product product : shoppingCart.getProductList()) {
-            BigDecimal productPrice = productService.getPriceWithDiscount(product);
+            var productPrice = productService.getPriceWithDiscount(product);
             totalPrice = totalPrice.add(productPrice);
         }
         return totalPrice;
