30a9d1737face94e2c935086506f8e4e691a4e5c
Maksim Shelkovich <6666350@gmail.com>
2021-09-07 22:59:55 +0300

    907: adds lecture materials

---
+++ lecturer/907_string/src/main/java/com/tutrit/java/quickstart/bean/Human.java
@@ -0,0 +1,13 @@
+package com.tutrit.java.quickstart.bean;
+
+public class Human {
+    String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
