a65b1346620d5b723e3a78d37aa436413408b91d
KaydunovDenis <deniskaydunov@gmail.com>
2021-10-07 10:25:52 +0300

    Minor refactoring and upgrade my cod

--- homework/kaydunov_denis/spring-aop/src/main/java/github/kaydunovdenis/App.java
+++ homework/kaydunov_denis/spring-aop/src/main/java/github/kaydunovdenis/App.java
@@ -1,25 +1,10 @@
 package github.kaydunovdenis;
 
-import github.kaydunovdenis.aspects.Logging;
-import github.kaydunovdenis.service.HumanService;
-import github.kaydunovdenis.service.UserServiceI;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
-        UserServiceI userService = (UserServiceI) ctx.getBean("UserService");
-        HumanService humanService = (HumanService) ctx.getBean("HumanService");
-        Logging logging = (Logging) ctx.getBean("Log");
-
-        userService.createUser();
-        userService.updateUser();
-        try {
-            userService.riskyOperationOnUser();
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-        }
-        userService.simpleOperationOnUser("userName", "userNic");
+        SpringConfiguration configuration = new SpringConfiguration();
+        configuration.init(new ClassPathXmlApplicationContext("spring-config.xml"));
     }
 }
