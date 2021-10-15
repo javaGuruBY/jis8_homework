d391f4188f34e06c89dc0c1bb921fbfeea17092e
lamaconda <kennethoffi6.6@gmail.com>
2021-10-15 20:39:03 +0300

    initial commit

---
+++ homework/shelkovich_ilya/servlet_code_repeat/src/main/java/com/tutrit/java/quickstart/Application.java
@@ -0,0 +1,13 @@
+package com.tutrit.java.quickstart;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static Logger log = LoggerFactory.getLogger("main");
+
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
+
