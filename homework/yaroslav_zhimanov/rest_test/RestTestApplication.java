3b8ac80d10a5416d4c69f343c98cf99e74d2a04c
yaroslav_zh <yaroslavne@yahoo.com>
2021-10-31 18:25:33 +0300

    Add Rest Test. Maybe someone needs

---
+++ homework/yaroslav_zhimanov/rest_test/RestTestApplication.java
@@ -0,0 +1,13 @@
+package jis8.rest_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTestApplication.class, args);
+	}
+
+}
