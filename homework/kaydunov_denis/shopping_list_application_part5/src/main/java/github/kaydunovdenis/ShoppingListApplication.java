8002a364514be8f98bec7fe02c7abbeffb4de588
Denis Kaydunov <deniakaydunov@gmail,com>
2021-12-21 10:01:21 +0300

    refactoring ProductValidator, changing spring configuration, injection dependency inversion

--- homework/kaydunov_denis/shopping_list_application_part5/src/main/java/github/kaydunovdenis/ShoppingListApplication.java
+++ homework/kaydunov_denis/shopping_list_application_part5/src/main/java/github/kaydunovdenis/ShoppingListApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Denis Kaydunov
